4413298bcebf088c4262a43a4ef6f9c83b6b4bac
Filipe Fernandes <filipefirmino50@gmail.com>
2025-11-25 16:17:04 -0300

    Aula37 Exer7

--- Aula37Exer7.java
+++ Aula37Exer7.java
@@ -8,6 +8,33 @@ public class Aula37Exer7 {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		double X,Y;
+		
+		X = sc.nextDouble();
+		Y = sc.nextDouble();
+		
+		if(X > 0 && Y > 0) {
+			System.out.println("Q1");
+			
+		}else if(X < 0 && Y > 0) {
+			System.out.println("Q2");
+			
+		}else if(X < 0 && Y < 0) {
+			System.out.println("Q3");
+			
+		}else if(X > 0 && Y < 0) {
+			System.out.println("Q4");
+			
+		}else if(X == 0 && (Y > 0 || Y < 0)) {
+			System.out.println("Eixo X");
+			
+		}else if(Y == 0 && (X > 0 || X < 0)) {
+			System.out.println("Eixo Y");
+			
+		}else if(X == 0 && Y == 0) {
+			System.out.println("Origem");
+			
+		}
 		
 		sc.close();
 	
