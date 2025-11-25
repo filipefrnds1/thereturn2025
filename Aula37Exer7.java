74db2f7ebda8d39f9d5ff7501cb88d87a76a2c89
Filipe Fernandes <filipefirmino50@gmail.com>
2025-11-25 17:28:32 -0300

    Aula37 Exer7

--- Aula37Exer7.java
+++ Aula37Exer7.java
@@ -13,6 +13,7 @@ public class Aula37Exer7 {
 		X = sc.nextDouble();
 		Y = sc.nextDouble();
 		
+		/*
 		if(X > 0 && Y > 0) {
 			System.out.println("Q1");
 			
@@ -35,6 +36,31 @@ public class Aula37Exer7 {
 			System.out.println("Origem");
 			
 		}
+		*/
+		
+		if(X == 0.0 && Y == 0.0) {
+			System.out.println("Origem");
+			
+		}else if(X == 0.0) {
+			System.out.println("Eixo Y");
+			
+		}else if(Y == 0.0) {
+			System.out.println("Eixo X");
+			
+		}else if(X > 0.0 && Y > 0.0) {
+			System.out.println("Q1");
+			
+		}else if(X < 0.0 && Y > 0.0) {
+			System.out.println("Q2");
+			
+		}else if(X < 0.0 && Y < 0.0) {
+			System.out.println("Q3");
+			
+		}else {
+			System.out.println("Q4");
+			
+		}
+		
 		
 		sc.close();
 	
