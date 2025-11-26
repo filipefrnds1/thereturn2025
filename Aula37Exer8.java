2a4af451d0ef952ee22d8542f00c8a052715fe03
Filipe Fernandes <filipefirmino50@gmail.com>
2025-11-26 18:51:22 -0300

    Aula37 Continuação

---
+++ Aula37Exer8.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Aula37Exer8 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		 double salario,aux,imposto;
+		 
+		 salario = sc.nextDouble();
+		 
+		 if(salario > 4.500) {
+			 aux = salario;
+			 imposto = ((salario-4500)*0.28)+()
+		 }
+		
+		
+		sc.close();
+
+	}
+
+}
