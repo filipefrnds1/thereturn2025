8c8e67afc537fdb9c3798f34c98c90423fdfbb01
Filipe Fernandes <filipefirmino50@gmail.com>
2025-11-28 12:23:53 -0300

    Aula37 Exer8

--- Aula37Exer8.java
+++ Aula37Exer8.java
@@ -7,15 +7,27 @@ public class Aula37Exer8 {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		 double salario,aux,imposto;
+		 double salario,imposto;
 		 
 		 salario = sc.nextDouble();
 		 
-		 if(salario > 4.500) {
-			 aux = salario;
-			 imposto = ((salario-4500)*0.28)+()
+		 if(salario <= 2000) {
+			 System.out.println("Isento");
+			 
+		 }else if(salario <= 3000) {
+			 imposto = (salario - 2000) * 0.08;
+			 System.out.printf("R$ %.2f", imposto);
+			 
+		 }else if(salario <= 4500) {
+			 imposto = ((salario - 3000)*0.18) + (1000*0.08);
+			 System.out.printf("R$ %.2f", imposto);
+			 
+		 }else if(salario > 4500) {
+			 imposto = ((salario - 4500)*0.28) + (1500*0.18) + (1000*0.08);
+			 System.out.printf("R$ %.2f", imposto);
 		 }
-		
+        
+		 
 		
 		sc.close();
 
