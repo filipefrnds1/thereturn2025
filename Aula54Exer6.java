046f555d7794d2165904d9ec58e6252397ab6a5f
Filipe Fernandes <filipefirmino50@gmail.com>
2025-12-19 12:32:23 -0300

    Aula54 Exer6

---
+++ Aula54Exer6.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Aula54Exer6 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int N;
+		N = sc.nextInt();
+		
+		
+		for(int i = 1; i <= N; i++) {
+			if(N%i == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		
+		sc.close();
+		
+	}
+
+}
